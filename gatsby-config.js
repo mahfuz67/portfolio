bb29777f5b6d0e63018cbed7f2996e9ac0a8164b
Regedit-msc <efusanyaae@gmail.com>
2020-11-10 13:57:47 +0100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Ay's Portfolio`,
+        short_name: `Ayfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
