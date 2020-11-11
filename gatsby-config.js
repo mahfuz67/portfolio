da9c05a5015a96540f0b88a606190c419a5336eb
Regedit-msc <efusanyaae@gmail.com>
2020-11-11 22:25:44 +0100

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#02aab0`,
         display: `standalone`,
-        icon: 'src/images/android-chrome-512x512',
+        icon: 'src/images/android-chrome-192x192.png',
       },
     },
   ],
